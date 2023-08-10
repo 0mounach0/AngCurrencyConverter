2cbf0c4fb4d1db635a4c3af22e7f4182cac92cb3
Mouad MOUNACH <me.mounach@gmail.com>
2023-08-10 21:33:37 +0200

    Auto-generated commit [ci skip]

--- src/app/directives/currency-switch.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { CurrencySwitchDirective } from './currency-switch.directive';
-
-describe('CurrencySwitchDirective', () => {
-  it('should create an instance', () => {
-    const directive = new CurrencySwitchDirective();
-    expect(directive).toBeTruthy();
-  });
-});
